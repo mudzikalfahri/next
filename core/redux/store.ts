da893054e4c68a5321a887becb3ada5009ee5135
mudzikalfahri <alfahrimudzik@gmail.com>
2022-06-25 15:20:13 +0700

    feat: add persist

--- core/redux/store.ts
+++ core/redux/store.ts
@@ -1,33 +1,62 @@
-import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import {
+  Action,
+  configureStore,
+  ThunkAction,
+  combineReducers,
+} from "@reduxjs/toolkit";
 import { apiSlice } from "@/core/redux/api/apiSlice";
 import { authReducer } from "@/core/redux/slices/auth";
-import { getFromStorage } from "@/utils/localstorage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const token = getFromStorage("token");
-const user = getFromStorage("user");
+const rootReducer = combineReducers({
+  [apiSlice.reducerPath]: apiSlice.reducer,
+  auth: authReducer,
+});
 
-const initialState = {
-  token: token || null,
-  user: user || null,
-  isAuthenticated: !!token,
+const createStore = () => {
+  let store;
+  const isClient = typeof window !== "undefined";
+  if (isClient) {
+    const persistConfig = {
+      key: "authType",
+      storage,
+      whiteList: ["auth"],
+    };
+
+    store = configureStore({
+      reducer: persistReducer(persistConfig, rootReducer),
+      middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+        }).concat(apiSlice.middleware),
+    });
+
+    // eslint-disable-next-line no-underscore-dangle
+    store.__PERSISTOR = persistStore(store);
+  } else {
+    store = configureStore({
+      reducer: rootReducer,
+      middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
+    });
+  }
+
+  return store;
 };
 
-export const store = configureStore({
-  reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
-    auth: authReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
-  preloadedState: { auth: initialState },
-});
-
-store.subscribe(() => {
-  if (store.getState().auth.token !== null)
-    localStorage.setItem("token", store.getState().auth.token);
-  if (store.getState().auth.user !== null)
-    localStorage.setItem("user", JSON.stringify(store.getState().auth.user));
-});
+export const store = createStore();
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
+/* eslint-disable no-underscore-dangle */
 import { store } from "@/core/redux/store";
 import "@/styles/global.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Provider store={store}>
-    <Component {...pageProps} />
+    <PersistGate persistor={store.__PERSISTOR} loading={null}>
+      <Component {...pageProps} />
+    </PersistGate>
   </Provider>
 );
 
